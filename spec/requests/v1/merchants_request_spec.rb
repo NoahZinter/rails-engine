f41266620ef76c9c4572d29b797aebe781a9760d
NoahZinter <noah.zinter@gmail.com>
2021-07-16 20:46:12 -0600

    Test merchant show action

--- spec/requests/v1/merchants_request_spec.rb
+++ spec/requests/v1/merchants_request_spec.rb
@@ -10,11 +10,32 @@ describe 'Merchants Requests' do
       expect(response.status).to eq 200
 
       merchants = JSON.parse(response.body, symbolize_names: true)
-
+# Should only return 20. Return to pagination.
       expect(merchants.count).to eq 50
+
+      merchants.each do |merchant|
+        expect(merchant).to have_key(:id)
+        expect(merchant[:id]).is_a? Integer
+        expect(merchant).to have_key(:name)
+        expect(merchant[:name]).is_a? String
+      end
     end
   end
 
+  describe '#show' do
+    it 'returns a given merchant' do
+      id = create(:merchant).id
 
+      get "/api/v1/merchants/#{id}"
 
+      expect(response.status).to eq 200
+
+      merchant = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchant).to have_key(:id)
+      expect(merchant[:id]).is_a? Integer
+      expect(merchant).to have_key(:name)
+      expect(merchant[:name]).is_a? String
+    end
+  end
 end
