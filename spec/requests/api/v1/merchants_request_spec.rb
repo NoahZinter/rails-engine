a39818baa0aca35cd01fa7b2ba0af7064ab1b03c
NoahZinter <noah.zinter@gmail.com>
2021-07-20 17:07:50 -0600

    Refactor tests

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -3,9 +3,6 @@ require 'rails_helper'
 describe 'Merchants Requests' do
   describe '#index' do
     it 'returns list of merchants, default 20' do
-      last_id = create_list(:merchant, 20).last.id
-      create_list(:merchant, 60)
-
       get '/api/v1/merchants'
 
       expect(response.status).to eq 200
@@ -14,7 +11,7 @@ describe 'Merchants Requests' do
       merchants = merchants[:data]
 
       expect(merchants.count).to eq 20
-      expect(merchants.last[:id]).to eq last_id.to_s
+      expect(merchants.last[:id]).to eq '20'
       merchants.each do |merchant|
         expect(merchant).to have_key(:id)
         expect(merchant[:id]).is_a? String
@@ -26,9 +23,6 @@ describe 'Merchants Requests' do
     end
 
     it 'returns the same list if page set to 1' do
-      last_id = create_list(:merchant, 20).last.id
-      create_list(:merchant, 60)
-
       get '/api/v1/merchants?page=1'
 
       expect(response.status).to eq 200
@@ -37,7 +31,7 @@ describe 'Merchants Requests' do
       merchants = merchants[:data]
 
       expect(merchants.count).to eq 20
-      expect(merchants.last[:id]).to eq last_id.to_s
+      expect(merchants.last[:id]).to eq '20'
       merchants.each do |merchant|
         expect(merchant).to have_key(:id)
         expect(merchant[:id]).is_a? String
@@ -49,8 +43,7 @@ describe 'Merchants Requests' do
     end
 
     it 'returns the next 20 merchants' do
-      create_list(:merchant, 20)
-      first_id = create_list(:merchant, 60).first.id
+      create_list(:merchant, 80)
 
       get '/api/v1/merchants?page=2'
 
@@ -60,7 +53,7 @@ describe 'Merchants Requests' do
       merchants = merchants[:data]
 
       expect(merchants.count).to eq 20
-      expect(merchants.first[:id]).to eq first_id.to_s
+      expect(merchants.first[:id]).to eq '21'
       merchants.each do |merchant|
         expect(merchant).to have_key(:id)
         expect(merchant[:id]).is_a? String
@@ -72,17 +65,16 @@ describe 'Merchants Requests' do
     end
 
     it 'can return more merchants' do
-      create_list(:merchant, 20).last.id
-      create_list(:merchant, 60)
+      create_list(:merchant, 80)
 
-      get '/api/v1/merchants?per_page=80'
+      get '/api/v1/merchants?per_page=100'
 
       expect(response.status).to eq 200
 
       merchants = JSON.parse(response.body, symbolize_names: true)
       merchants = merchants[:data]
 
-      expect(merchants.count).to eq 80
+      expect(merchants.count).to eq 100
       merchants.each do |merchant|
         expect(merchant).to have_key(:id)
         expect(merchant[:id]).is_a? String
@@ -94,8 +86,7 @@ describe 'Merchants Requests' do
     end
 
     it 'does not return more merchants than exist' do
-      create_list(:merchant, 20).last.id
-      create_list(:merchant, 60)
+      create_list(:merchant, 80)
 
       get '/api/v1/merchants?per_page=500'
 
@@ -104,7 +95,7 @@ describe 'Merchants Requests' do
       merchants = JSON.parse(response.body, symbolize_names: true)
       merchants = merchants[:data]
 
-      expect(merchants.count).to eq 80
+      expect(merchants.count).to eq 100
       merchants.each do |merchant|
         expect(merchant).to have_key(:id)
         expect(merchant[:id]).is_a? String
