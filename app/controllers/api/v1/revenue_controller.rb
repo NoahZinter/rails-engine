a7a3a5e84423db23a054f189914115f10a72b2ef
NoahZinter <noah.zinter@gmail.com>
2021-07-21 13:15:15 -0600

    Test top item serialization

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -9,4 +9,15 @@ class Api::V1::RevenueController < ApplicationController
       render json: data, status: 400
     end
   end
+
+  def top_items
+    if params[:quantity] && (params[:quantity] != '')
+      items = Item.top_by_revenue(params[:quantity])
+      render json: ItemRevenueSerializer.new(items).serializable_hash.to_json
+    else
+      data = { errors: [] }
+      data[:errors] << {id: nil, message: 'invalid quantity'}
+      render json: data, status: 400
+    end
+  end
 end
--- spec/requests/revenue_request_spec.rb
+++ spec/requests/revenue_request_spec.rb
@@ -32,4 +32,33 @@ RSpec.describe 'Revenue Requests' do
       expect(error[:errors].first[:message]).to eq 'invalid quantity'
     end
   end
+
+  describe 'items' do
+    it 'returns top x items by revenue' do
+      get '/api/v1/revenue/items?quantity=5'
+
+      expect(response.status).to eq 200
+
+      items = JSON.parse(response.body, symbolize_names: true)
+      items = items[:data]
+
+      expect(items.count).to eq 5
+      items.each do |item|
+        expect(item).to have_key(:id)
+        expect(item[:id]).is_a? String
+        expect(item).to have_key(:type)
+        expect(item[:type]).to eq 'item_revenue'
+        expect(item[:attributes]).to have_key(:name)
+        expect(item[:attributes][:name]).is_a? String
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).is_a? String
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).is_a? Float
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).is_a? Integer
+        expect(item[:attributes]).to have_key(:revenue)
+        expect(item[:attributes][:revenue]).is_a? Float
+      end
+    end
+  end
 end
