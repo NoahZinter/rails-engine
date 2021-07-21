6956d07cb866b9722c389dde62694e5523643059
NoahZinter <noah.zinter@gmail.com>
2021-07-21 13:38:06 -0600

    Refactor top items to default to 10 items

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -11,13 +11,16 @@ class Api::V1::RevenueController < ApplicationController
   end
 
   def top_items
-    if params[:quantity] && (params[:quantity] != '')
-      items = Item.top_by_revenue(params[:quantity])
-      render json: ItemRevenueSerializer.new(items).serializable_hash.to_json
-    else
+    if params[:quantity].to_i < 0
       data = { errors: [] }
       data[:errors] << {id: nil, message: 'invalid quantity'}
       render json: data, status: 400
+    elsif params[:quantity] && (params[:quantity] != '')
+      items = Item.top_by_revenue(params[:quantity])
+      render json: ItemRevenueSerializer.new(items).serializable_hash.to_json
+    else
+      items = Item.top_by_revenue(10)
+      render json: ItemRevenueSerializer.new(items).serializable_hash.to_json
     end
   end
 end
